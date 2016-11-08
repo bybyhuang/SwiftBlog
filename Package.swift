2ab3520b5525c915c07b9d219e492aac1beb1288
huangbaoyu <huang_baoyu@163.com>
2016-11-08 11:16:11 +0800

    生成项目文件

---
+++ Package.swift
@@ -0,0 +1,18 @@
+//
+//  CommentImagesView.swift
+//  ChaChong
+//
+//  Created by huangbaoyu on 16/11/8.
+//  Copyright © 2016年 ChaChong. All rights reserved.
+//
+
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftBlog",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0)
+    ]
+)
